3754aa77a1d98cd94e6ab0bf6565cc3e15cb10e7
igorssobral <igorgmz1421@gmail.com>
2024-09-25 22:08:41 -0300

    .

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -47,7 +47,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-sm text-muted", className)}
+    className={cn("text-base text-muted", className)}
     {...props}
   />
 ))
